7d84bb8414a1de9feb07223a2460747a8d1d85f8
shinieaggarwal72 <shinieaggarwal72@gmail.com>
2025-07-07 01:09:42 +0530

    minor changes

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const navLinks = document.getElementById('nav-links');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 });
+
+const text = "Shinie Aggarwal.";
+let index = 0;
+const typedSpan = document.querySelector('.typed-text');
+
+function type() {
+    if (index < text.length) {
+        typedSpan.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 100);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', type);
+
